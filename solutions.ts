776178d17515d29e8ccfbcee96086563fa40a40d
COMRADE040 <rafiulislam040@gmail.com>
2026-05-07 00:54:38 +0600

    reverseString with typescript

--- solutions.ts
+++ solutions.ts
@@ -3,3 +3,18 @@ const filterEvenNumbers = (numbers: number[]): number[] => {
     return numbers.filter((num) => num % 2 === 0)
 }
 console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6]))
+
+
+
+
+// Reverse String
+const reverseString = (str: string): string => {
+    let result = "";
+    for(let i = str.length - 1; i >= 0; i--){
+        result = result + str[i]
+    }
+    return result
+}
+console.log(reverseString("typescript"))
+
+
